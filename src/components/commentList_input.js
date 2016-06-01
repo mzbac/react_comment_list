06a5f2b944923a2008fef56adee9315fc31ff072
anchen.li <li.anchen.au@gmail.com>
2016-06-01 20:38:05 +1000

    comment input alignment fix

--- src/components/commentList_input.js
+++ src/components/commentList_input.js
@@ -1,10 +1,17 @@
 import React from 'react';
+
+const inputStyle={
+    'marginLeft':'-1px'
+}
+const iconStyle={
+    'borderRight':'0px'
+}
 const CommentInput = (props) => {
     return (
         <div className="input-group">
-            <input type="text" className="form-control no-border-radius" value={props.value } onChange={props.onChange}
+            <input type="text" style={inputStyle} className="form-control no-border-radius" value={props.value } onChange={props.onChange}
                    placeholder="Type a comment" aria-describedby="basic-addon2"/>
-            <span className="input-group-addon no-border-radius" onClick={props.onSubmit}><i
+            <span className="input-group-addon no-border-radius" style={iconStyle}  onClick={props.onSubmit}><i
                 className="fa fa-paper-plane" aria-hidden="true"></i></span>
         </div>
     );
